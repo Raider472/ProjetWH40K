314b9b5282b2d8fedc2de72823899758fa5e621b
Raider <gabrielnae1@gmail.com>
2023-02-06 20:57:30 +0100

    Avancement de la BDD

--- src/ScriptBDD.sql
+++ src/ScriptBDD.sql
@@ -2,6 +2,8 @@ SET FOREIGN_KEY_CHECKS=0;
 
 -- Suppression des Tableaux
 drop table if exists SortPris;
+drop table if exists UnitéPrise;
+drop table if exists Liste;
 drop table if exists Sort;
 drop table if exists Aptitude;
 drop table if exists Unité;
@@ -59,17 +61,9 @@ CREATE TABLE Sort (
     id_sousFaction int DEFAULT NULL,
     nom_sort varchar(255) NOT NULL,
     desc_sort varchar(255) DEFAULT NULL,
+    type_sort varchar(255) DEFAULT NULL,
     FOREIGN KEY(id_faction) REFERENCES Faction(num_faction),
     FOREIGN KEY(id_sousFaction) REFERENCES SousFaction(num_sousFaction)
-);
-
--- -----------------------
-
-CREATE TABLE SortPris (
-    id_unitchoisie int NOT NULL,
-    id_sort int NOT NULL,
-    PRIMARY KEY(id_unitchoisie, id_sort),
-    FOREIGN KEY(id_sort) REFERENCES Sort(num_sort)
 );
 
 -- ------------------------
@@ -90,16 +84,42 @@ CREATE TABLE Unité (
     FOREIGN KEY(sousFaction_unit) REFERENCES SousFaction(num_sousFaction)
 );
 
+-- -----------------------
+CREATE TABLE Liste (
+    num_liste int PRIMARY KEY NOT NULL,
+    nom_liste varchar(255) DEFAULT NULL
+);
+
 -- ------------------------
 CREATE TABLE UnitéPrise (
-    numéro_unit int NOT NULL,
     id_unitPris int NOT NULL AUTO_INCREMENT,
-    -------
+    id_liste int NOT NULL,
+    numéro_unit int NOT NULL,
+    sousFaction_unit int NOT NULL,
+    trait_unit int NOT NULL,
+    nom_unit varchar(50) NOT NULL,
+    nomFaction_unit varchar(50) NOT NULL,
+    point_unit int NOT NULL DEFAULT 0,
+    nombre_unit int NOT NULL,
+    PRIMARY KEY(id_unitPris, id_liste),
+    FOREIGN KEY(id_liste) REFERENCES Liste(num_liste),
+    FOREIGN KEY(numéro_unit) REFERENCES Unité(numéro_unit),
+    FOREIGN KEY(trait_unit) REFERENCES Trait(num_trait),
+    FOREIGN KEY(sousFaction_unit) REFERENCES SousFaction(num_sousFaction)
+);
+
+-- -----------------------
+CREATE TABLE SortPris (
+    id_unitchoisie int NOT NULL,
+    id_sort int NOT NULL,
+    PRIMARY KEY(id_unitchoisie, id_sort),
+    FOREIGN KEY(id_sort) REFERENCES Sort(num_sort),
+    FOREIGN KEY(id_unitchoisie) REFERENCES UnitéPrise(id_unitPris)
 );
 
 -- Insértion dans les tableaux
 -- Insertion Des Factions
-INSERT INTO Faction(nom_faction, alliance_faction) VALUES ("Nécrons", "Xeno"), ("Thousand Sons", "Chaos");
+INSERT INTO Faction(nom_faction, alliance_faction) VALUES ("Nécrons", "Xeno"), ("Thousand Sons", "Chaos"), ("Ork", "Xeno");
 -- Insertion Des Sous Factions
 INSERT INTO SousFaction(id_faction, nom_sousFaction) VALUES (1, "Neutre");
 INSERT INTO SousFaction(id_faction, nom_sousFaction, nom_faction) SELECT num_faction, "Novokh", nom_faction FROM Faction WHERE num_faction = 1; 
