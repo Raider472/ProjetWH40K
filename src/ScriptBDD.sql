e4ba2cba3ff9a4b001bb19661c1e3059d9d08ae8
Raider <gabrielnae1@gmail.com>
2023-02-07 20:22:52 +0100

    Ajout des traits dynastique

--- src/ScriptBDD.sql
+++ src/ScriptBDD.sql
@@ -5,6 +5,7 @@ drop table if exists SortPris;
 drop table if exists UnitéPrise;
 drop table if exists Liste;
 drop table if exists Sort;
+drop table if exists Reliques;
 drop table if exists AptitudeLiaison;
 drop table if exists Aptitude;
 drop table if exists Unité;
@@ -47,7 +48,7 @@ CREATE TABLE Aptitude (
     id_faction int NOT NULL,
     id_sousFaction int DEFAULT NULL,
     nom_apti varchar(255) NOT NULL,
-    desc_apti varchar(255) DEFAULT NULL,
+    desc_apti varchar(1000) DEFAULT NULL,
     FOREIGN KEY(id_faction) REFERENCES Faction(num_faction),
     FOREIGN KEY(id_sousFaction) REFERENCES SousFaction(num_sousFaction)
 );
@@ -95,6 +96,16 @@ CREATE TABLE AptitudeLiaison (
 );
 
 -- -----------------------
+CREATE TABLE Reliques (
+    num_relique int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_faction int NOT NULL,
+    nom_relique varchar(255) NOT NULL,
+    desc_relique varchar(255) DEFAULT NULL,
+    type_relique varchar(255) NOT NULL,
+    FOREIGN KEY(id_faction) REFERENCES Faction(num_faction)
+);
+
+-- -----------------------
 CREATE TABLE Liste (
     num_liste int PRIMARY KEY NOT NULL,
     nom_liste varchar(255) DEFAULT NULL
@@ -107,6 +118,7 @@ CREATE TABLE UnitéPrise (
     numéro_unit int NOT NULL,
     sousFaction_unit int NOT NULL,
     trait_unit int NOT NULL,
+    id_relique int DEFAULT NULL,
     nom_unit varchar(50) NOT NULL,
     nomFaction_unit varchar(50) NOT NULL,
     point_unit int NOT NULL DEFAULT 0,
@@ -115,7 +127,8 @@ CREATE TABLE UnitéPrise (
     FOREIGN KEY(id_liste) REFERENCES Liste(num_liste),
     FOREIGN KEY(numéro_unit) REFERENCES Unité(numéro_unit),
     FOREIGN KEY(trait_unit) REFERENCES Trait(num_trait),
-    FOREIGN KEY(sousFaction_unit) REFERENCES SousFaction(num_sousFaction)
+    FOREIGN KEY(sousFaction_unit) REFERENCES SousFaction(num_sousFaction),
+    FOREIGN KEY(id_relique) REFERENCES Reliques(num_relique)
 );
 
 -- -----------------------
@@ -169,9 +182,16 @@ INSERT INTO Trait(id_faction, nom_trait, desc_trait)
 VALUES (1, "Volonté immuable", "Chaque fois qu'on alloue une attaque à ce Seigneur de Guerre, on soustrait 1 à la caractéristique de Dégats de l'attaque (jusqu'à un minimum de 1)."),
 (1, "Folie éternelle", "Chaque fois que ce Seigneur de Guerre fait une attaque de mêlée, vous pouvez relancer le jet de blessure"),
 (1, "Vanité imortelle", "Chaque fois que ce Seigneur de Guerre est censé perdre un PV suite à une blessure mortelle, jetez un D6: sur un 5+, le PV n'est pas perdu. \n Ce Seigneur de Guerre a l'aptitude suivante: Vanité immortelle (aura): Tant qu'une unité Base<Dynastie> amie est à 6 pouce de cette figurine, chaque fois qu'un test d'attrition est effectué pour l'unité, ignorez-en certains ou tous les modificateurs."),
-(1, "Servant du roi silencieux", "Ajoutez 3 pouce à la portée des aptitudes d'aura de ce Seigneur de Guerre (max 9). De plus, quand ce Seigneur de Guerre utilise les aptitudes Que Ma Volonté s'Acomplisse, Volonté du Dynaste ou Adaptation Stratégique, vous pouvez choisir une unité Base <Dynastie> amie à 12 pouce de ce Seigneur de Guerre au lieu de 9 pouce.");
+(1, "Servant du roi silencieux", "Ajoutez 3 pouce à la portée des aptitudes d'aura de ce Seigneur de Guerre (max 9). De plus, quand ce Seigneur de Guerre utilise les aptitudes Que Ma Volonté s'Acomplisse, Volonté du Dynaste ou Adaptation Stratégique, vous pouvez choisir une unité Base <Dynastie> amie à 12 pouce de ce Seigneur de Guerre au lieu de 9 pouce."),
+(1, "Conquérent implacable(Aura)", "Tant qu'une unité Base<Dynastie> amie est à 6 pouce de ce Seigneur de Guerre, vous pouvez relancer les jets de charges pour l'unité."),
+(1, "Combattant honorable", "A la phase de combat, chaque fois que ce Seigneur de Guerre est choisi pour combattre, il peut engager un combat honorable. Dans ce cas, choisissez une unité Personnage ennemie; jusqu'à la fin de la phase ajoutez 2 à la caractéristique d'Attaque de ce Seigneur de Guerre, mais il peut seulement faire des attaques qui ciblent l'unité Personnage ennemie.");
 -- Insertion des Aptitudes
-INSERT INTO Aptitude(id_faction, id_sousFaction, nom_apti) VALUES (1, 2, "Test");
-INSERT INTO Aptitude(id_faction, nom_apti) VALUES (1, "Test2");
+INSERT INTO Aptitude(id_faction, id_sousFaction, nom_apti, desc_apti) VALUES (1, 2, "Eveil par le meurtre", "Ajoutez 1 aux jets de charge pour les unités ayant ce code. \n Chaque fois qu'une figurine ayant ce code fait une attaque de mêlée, si l'unité de la figurine a fait un mouvement de charge, a été chargée ou a accompli une Intervention Héroique à ce tour, améliorez de 1 la caractéristique de Pénétration d'armure de l'attaque. \n Lorsque le protocole du Néant Avide devient actif pour votre armée, si toutes les unité de votre armée (à l'exception des unités DYNASTIC AGENT et C'TAN SHARD) possède ce code, vous pouvez sélectionner les deux directives de ce protocole de commande au lieu d'une seule."),
+(1, 3, "Artificiers remarquables", "Chaque fois qu'une figurine ayant ce code perdrait un PV suite à une blessure mortelle, lancez un D6 ; sur un 5+, le PV n'est pas perdue. \n Chaque fois qu'une unité ayant ce code est sélectionnée pour tirer ou combattre, vous pouvez relancer un seul jet de blessure en faisant les attaques de l'unité. \n Quand le Protocole des Légions Impérissable devient actif pour votre armée, si toutes les unités de votre armée (à l'exception des unités D'AGENT DYNASTIQUE et d'ECHARDE C'TAN) possède ce code, vous pouvez sélectionner les deux directives de ce protocole de commandement au lieu d'une seule."),
+(1, 4, "Fureur Solaire", "Ajoutez 3 pouce à la caractéristique de Portée des armes de tir (à l'exception des pistolets) dont les figurines possédant ce code sont équipées. \n Chaque fois qu'une figurine possédant ce code effectue une attaque à distance qui cible une unité située à mi-distance, la caractéristique de pénétration de l'armure de cette attaque est améliorée de 1. \n Quand le protocole des Astres Vengeurs devient actif pour votre armée, si toutes les unités de votre armée (à l'exception des unités de l'AGENT DYNASTIQUE et du C'TAN SHARD) possèdent ce code, vous pouvez sélectionner les deux directives de ce protocole de commandement au lieu d'une seule."),
+(1, 5, "Rayons de translocalisation", "Les figurines avec ce code ont une sauvegarde invulnérable de 6+. \n Chaque fois qu'une unité avec ce code Avance, elle peut se translocaliser. Si elle le fait, ne faites pas de jet d'Avance pour elle. A la place, jusqu'à la fin de la phase, ajoutez 6 pouce à la caractéristique de mouvement des figurines de cette unité. Si une unité se translocalise, jusqu'à la fin du tour, les figurines de cette unité ne peuvent pas tirer. \n Chaque fois qu'une unité avec ce code Bat en Retraite ou se translocalise, jusqu'à la fin de la phase, les figurines de cette unité peuvent se déplacer à travers les figurines et le terrain comme s'ils n'étaient pas là. \n Quand le Protocole de la Tempête Soudaine devient actif pour votre armée, si chaque unité de votre armée (à l'exception des unités de l'AGENT DYNASTIQUE et de la CARTE C'TAN) possède ce code, vous pouvez sélectionner les deux directives de ce protocole de commandement au lieu d'une seule."),
+(1, 6, "Terrritorialistes agressifs", "Les unités avec ce code ont la capacité Objectif Sécurisé. Si une figurine d'une telle unité possède déjà cette aptitude, la figurine compte comme une figurine supplémentaire lors de la détermination du contrôle d'un pion d'objectif. \n Chaque fois qu'une attaque ayant une caractéristique de pénétration d'armure de - 1 est allouée à une figurine ayant ce code, si l'unité de la figurine est entièrement dans la zone de déploiement de son contrôleur, cette attaque a une caractéristique de pénétration d'armure de 0 à la place. \n Quand le Protocole du Gardien Éternel devient actif pour votre armée, si chaque unité de votre armée (à l'exception des unités de l'AGENT DYNASTIQUE et du C'TAN SHARD) possède ce code, vous pouvez sélectionner les deux directives de ce protocole de commandement au lieu d'une seule."),
+(1, 7, "Avancé implacable", "Chaque fois qu'un test de Moral est effectué pour une unité avec ce code, vous pouvez relancer ce test. \n Au lieu de suivre les règles normales pour les armes à tir rapide, les figurines ayant ce code tirant avec des armes à tir rapide effectuent deux fois plus d'attaques si la cible de la figurine qui tire est à 18 pouce. \n Quand le Protocole du Tyran Conquérant devient actif pour votre armée, si toutes les unités de votre armée (à l'exception des unités de l'AGENT DYNASTIQUE et du C'TAN SHARD) possèdent ce code, vous pouvez sélectionner les deux directives de ce protocole de commandement au lieu d'une seule.");
+INSERT INTO Aptitude(id_faction, nom_apti, desc_apti) VALUES (1, "Protocole de réanimation", "Chaque fois que les protocoles de réanimation d'une unité se déclenche, effectuez des jets de protocole de réanimation pour cette unité en lançant un nombre de D6 égal à la caractéristiques de PV combinée de toutes les figurines en train de se réassembler. Chaque jet de protocole de réanimation de 5+ est placé dans un pool. Un jet de protocole de réanimation ne peut jamais être modifié par plus de -1 ou +1.");
 -- Insertion des sorts
 INSERT INTO Sort(id_faction, nom_sort) VALUES (2, "Death");
