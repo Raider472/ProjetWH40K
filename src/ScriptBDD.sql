25c39667568fa2c50720eec24090b4d8d18bc79a
Raider <gabrielnae1@gmail.com>
2023-02-09 16:33:13 +0100

    Correction Script + ajout

--- src/ScriptBDD.sql
+++ src/ScriptBDD.sql
@@ -98,27 +98,29 @@ CREATE TABLE Unité (
 
 -- -------------------------
 CREATE TABLE Arme (
-    num_arme int NOT NULL AUTO_INCREMENT,
-    id_unit int NOT NULL,
+    num_arme int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nom_arme varchar(255) NOT NULL,
+    id_faction int NOT NULL,
     portée_arme int DEFAULT NULL,
     melée_arme BOOLEAN NOT NULL,
+    type_arme varchar(255) NOT NULL,
     force_arme int NOT NULL,
     pa_arme int NOT NULL,
-    dégat_arme int NOT NULL,
+    dégat_arme varchar(255) NOT NULL,
     aptitude_arme varchar(500) DEFAULT NULL,
     point_arme int DEFAULT NULL,
-    unique_arme BOOLEAN DEFAULT true,
+    switchable_arme BOOLEAN DEFAULT false,
     arme_relique BOOLEAN DEFAULT false, 
     trancheFig_arme int DEFAULT NULL,
-    PRIMARY KEY(num_arme, id_unit),
-    FOREIGN KEY(id_unit) REFERENCES Unité(numéro_unit)
+    FOREIGN KEY(id_faction) REFERENCES Faction(num_faction)
 );
 
 -- -------------------------
 CREATE TABLE ArmeLiaison (
     id_arme int NOT NULL,
     id_unit int NOT NULL,
+    nom_arme varchar(255) DEFAULT NULL,
+    nom_unit varchar(255) DEFAULT NULL,
     PRIMARY KEY(id_arme, id_unit),
     FOREIGN KEY(id_arme) REFERENCES Arme(num_arme),
     FOREIGN KEY(id_unit) REFERENCES Unité(numéro_unit)
@@ -148,7 +150,7 @@ CREATE TABLE Reliques (
 -- -----------------------
 CREATE TABLE Liste (
     num_liste int PRIMARY KEY NOT NULL,
-    nom_liste varchar(255) DEFAULT NULL
+    nom_liste varchar(255) NOT NULL
 );
 
 -- ------------------------
@@ -194,8 +196,10 @@ CREATE TABLE ArmePrise (
 );
 
 -- Insértion dans les tableaux
+
 -- Insertion Des Factions
 INSERT INTO Faction(nom_faction, alliance_faction) VALUES ("Nécrons", "Xeno"), ("Thousand Sons", "Chaos"), ("Ork", "Xeno");
+
 -- Insertion Des Sous Factions
 INSERT INTO SousFaction(id_faction, nom_sousFaction, nom_faction) VALUES (1, "Neutre", "Nécrons");
 INSERT INTO SousFaction(id_faction, nom_sousFaction, nom_faction) SELECT num_faction, "Novokh", nom_faction FROM Faction WHERE num_faction = 1; 
@@ -204,6 +208,7 @@ INSERT INTO SousFaction(id_faction, nom_sousFaction, nom_faction) SELECT num_fac
 INSERT INTO SousFaction(id_faction, nom_sousFaction, nom_faction) SELECT num_faction, "Nephrekh", nom_faction FROM Faction WHERE num_faction = 1;
 INSERT INTO SousFaction(id_faction, nom_sousFaction, nom_faction) SELECT num_faction, "Nihilakh", nom_faction FROM Faction WHERE num_faction = 1;
 INSERT INTO SousFaction(id_faction, nom_sousFaction, nom_faction) SELECT num_faction, "Sautekh", nom_faction FROM Faction WHERE num_faction = 1;
+
 -- Insertion des traits
 INSERT INTO Trait(id_faction, nom_trait, id_sousFaction, desc_trait) 
 SELECT id_faction, "Fureur de sang", num_sousFaction, "Chaque fois que ce Seigneur de guerre fait une attaque de mêlée, un jet de blessure non modifié de 6 inflige 1 blessure mortelle à la cible en plus de tout autre dégât normal."
@@ -236,6 +241,7 @@ VALUES (1, "Volonté immuable", "Chaque fois qu'on alloue une attaque à ce Seig
 (1, "Servant du roi silencieux", "Ajoutez 3 pouce à la portée des aptitudes d'aura de ce Seigneur de Guerre (max 9). De plus, quand ce Seigneur de Guerre utilise les aptitudes Que Ma Volonté s'Acomplisse, Volonté du Dynaste ou Adaptation Stratégique, vous pouvez choisir une unité Base <Dynastie> amie à 12 pouce de ce Seigneur de Guerre au lieu de 9 pouce."),
 (1, "Conquérent implacable(Aura)", "Tant qu'une unité Base<Dynastie> amie est à 6 pouce de ce Seigneur de Guerre, vous pouvez relancer les jets de charges pour l'unité."),
 (1, "Combattant honorable", "A la phase de combat, chaque fois que ce Seigneur de Guerre est choisi pour combattre, il peut engager un combat honorable. Dans ce cas, choisissez une unité Personnage ennemie; jusqu'à la fin de la phase ajoutez 2 à la caractéristique d'Attaque de ce Seigneur de Guerre, mais il peut seulement faire des attaques qui ciblent l'unité Personnage ennemie.");
+
 -- Insertion des Aptitudes
 INSERT INTO Aptitude(id_faction, id_sousFaction, nom_apti, desc_apti) VALUES (1, 2, "Eveil par le meurtre", "Ajoutez 1 aux jets de charge pour les unités ayant ce code. \n Chaque fois qu'une figurine ayant ce code fait une attaque de mêlée, si l'unité de la figurine a fait un mouvement de charge, a été chargée ou a accompli une Intervention Héroique à ce tour, améliorez de 1 la caractéristique de Pénétration d'armure de l'attaque. \n Lorsque le protocole du Néant Avide devient actif pour votre armée, si toutes les unité de votre armée (à l'exception des unités DYNASTIC AGENT et C'TAN SHARD) possède ce code, vous pouvez sélectionner les deux directives de ce protocole de commande au lieu d'une seule."),
 (1, 3, "Artificiers remarquables", "Chaque fois qu'une figurine ayant ce code perdrait un PV suite à une blessure mortelle, lancez un D6 ; sur un 5+, le PV n'est pas perdue. \n Chaque fois qu'une unité ayant ce code est sélectionnée pour tirer ou combattre, vous pouvez relancer un seul jet de blessure en faisant les attaques de l'unité. \n Quand le Protocole des Légions Impérissable devient actif pour votre armée, si toutes les unités de votre armée (à l'exception des unités D'AGENT DYNASTIQUE et d'ECHARDE C'TAN) possède ce code, vous pouvez sélectionner les deux directives de ce protocole de commandement au lieu d'une seule."),
@@ -247,21 +253,63 @@ INSERT INTO Aptitude(id_faction, nom_apti, desc_apti) VALUES (1, "Protocole de r
 (1, "Métal organique", "Au début de votre phase de Commandement, chaque figurine de cette unité regagne 1 PV perdue."),
 (1, "Translocation dimensionnelle", "Au déploiement, vous pouvez placer cette unité dans une dimension hyperspatiale au lieu de la placer sur le champ de bataille. Dans ce cas, lors de l'étape Renforts de l'une de vos phases de mouvement, vous pouvez placer cette unité n'importe où sur le champ de bataille à une distance de plus de 9 pouce de toute figurine ennemi."),
 (1, "Rites de réanimation", "À votre phase de commandement, vous pouvez choisir une unité amie NECRONS BASE à 6 pouce de cette figurine. Une figurine détruit de cette unité est réanimé. Si l'unité sélectionnée est une unité Guerrier Nécrons, D3 modèles détruits de cette unité sont réanimés à la place. Chaque unité ne peut être sélectionnée pour cette capacité qu'une fois par phase."),
-(1 , "Surchargeur Empyréen (Aura)", "Tant qu'une unité de PSYKER ennemie est à 12 pouce de cette figurine, à chaque fois qu'un test de Psychique est effectué pour cette unité, elle subit les Périls du Warp sur tout jet de dé qui inclut un double, au lieu d'un double 1 ou un double 6."),
-(1 , "Manipulateur d'énergie atomique", "Lors de la phase de combat, si cette figurine détruit un ou plusieurs figurines ennemies, alors à la fin de cette phase il peut utiliser sa capacité Augmentation mécanique comme si c'était la fin de votre phase de mouvement."),
+(1, "Surchargeur Empyréen (Aura)", "Tant qu'une unité de PSYKER ennemie est à 12 pouce de cette figurine, à chaque fois qu'un test de Psychique est effectué pour cette unité, elle subit les Périls du Warp sur tout jet de dé qui inclut un double, au lieu d'un double 1 ou un double 6."),
+(1, "Manipulateur d'énergie atomique", "Lors de la phase de combat, si cette figurine détruit un ou plusieurs figurines ennemies, alors à la fin de cette phase il peut utiliser sa capacité Augmentation mécanique comme si c'était la fin de votre phase de mouvement."),
 (1, "Augmentation mécanique", "A la fin de votre phase de mouvement, vous pouvez sélectionner une unité amie NECRONS BASE à 6 pouce de cette figurine. Si vous le faites, lancez un D3 et consultez le tableau ci-dessous : \n 1. Jusqu'à la fin de la bataille, ajoutez 1 à la caractéristique de Force des figurines de cette unité. \n 2. Jusqu'à la fin de la bataille, ajoutez 1 à la caractéristique d'endurance des figurines de cette unité. \n 3. Jusqu'à la fin de la bataille, améliorez de 1 la caractéristique de capacité de tir des figurines de cette unité."),
 (1, "Illuminor", "Cette figurine peut utiliser sa capacité Rites de réanimation une fois de plus par tour.");
+
 -- Insertion des Unités
 INSERT INTO Unité(faction_id_unit, sousFaction_unit, personnage_unit, trait_unit, nom_unit, nomFaction_unit, point_unit, min_unit, max_unit, mouvement_unit, cc_unit, ct_unit, force_unit, endurance_unit, pv_unit, attaque_unit, cd_unit, sv_unit)
 SELECT num_faction, 1, true, 7, "Illuminor Szeras", nom_faction, 160, 1, 1, 8, 3, 3, 6, 6, 7, 4, 10, 3
 FROM Faction 
 WHERE num_faction = 1;
--- Insertion des armes
 
--- Insertion des Aptitudes
+-- Insertion des armes
+INSERT INTO Arme(nom_arme, id_faction, portée_arme, melée_arme, type_arme, force_arme, pa_arme, dégat_arme)
+VALUES("Lance Spectrale (tir)", 1, 36, false, "Assaut D3", 8, -4, "D6");
+INSERT INTO Arme(nom_arme, id_faction, melée_arme, type_arme, force_arme, pa_arme, dégat_arme)
+VALUES("Lance Spectrale (mêlée)", 1, true, "mêlée", 6, -4, "2");
+INSERT INTO Arme(nom_arme, id_faction, melée_arme, type_arme, force_arme, pa_arme, dégat_arme, aptitude_arme)
+VALUES("Jambes empaleuses", 1, true, "mêlée", 6, -2, "1", "Chaque fois que le porteur combat, il fait 2 attaques supplémentaire avec cette arme.");
+-- Insertion des Liaisons d'armes
+INSERT INTO ArmeLiaison(id_arme, id_unit, nom_arme, nom_unit)
+SELECT num_arme, numéro_unit, nom_arme, nom_unit
+FROM Arme, Unité
+WHERE nom_unit = "Illuminor Szeras" AND nom_arme = "Lance Spectrale (tir)";
+INSERT INTO ArmeLiaison(id_arme, id_unit, nom_arme, nom_unit)
+SELECT num_arme, numéro_unit, nom_arme, nom_unit
+FROM Arme, Unité
+WHERE nom_unit = "Illuminor Szeras" AND nom_arme = "Lance Spectrale (mêlée)";
+INSERT INTO ArmeLiaison(id_arme, id_unit, nom_arme, nom_unit)
+SELECT num_arme, numéro_unit, nom_arme, nom_unit
+FROM Arme, Unité
+WHERE nom_unit = "Illuminor Szeras" AND nom_arme = "Jambes empaleuses";
+
+-- Insertion des Liaisons d'Aptitudes
 INSERT INTO AptitudeLiaison(id_apti, id_unit, nom_apti, nom_unit)
 SELECT num_apti, numéro_unit, nom_apti, nom_unit
 FROM Aptitude, Unité
 WHERE nom_unit = "Illuminor Szeras" AND nom_apti = "Métal organique";
+INSERT INTO AptitudeLiaison(id_apti, id_unit, nom_apti, nom_unit)
+SELECT num_apti, numéro_unit, nom_apti, nom_unit
+FROM Aptitude, Unité
+WHERE nom_unit = "Illuminor Szeras" AND nom_apti = "Rites de réanimation";
+INSERT INTO AptitudeLiaison(id_apti, id_unit, nom_apti, nom_unit)
+SELECT num_apti, numéro_unit, nom_apti, nom_unit
+FROM Aptitude, Unité
+WHERE nom_unit = "Illuminor Szeras" AND nom_apti = "Surchargeur Empyréen (Aura)";
+INSERT INTO AptitudeLiaison(id_apti, id_unit, nom_apti, nom_unit)
+SELECT num_apti, numéro_unit, nom_apti, nom_unit
+FROM Aptitude, Unité
+WHERE nom_unit = "Illuminor Szeras" AND nom_apti = "Manipulateur d'énergie atomique";
+INSERT INTO AptitudeLiaison(id_apti, id_unit, nom_apti, nom_unit)
+SELECT num_apti, numéro_unit, nom_apti, nom_unit
+FROM Aptitude, Unité
+WHERE nom_unit = "Illuminor Szeras" AND nom_apti = "Augmentation mécanique";
+INSERT INTO AptitudeLiaison(id_apti, id_unit, nom_apti, nom_unit)
+SELECT num_apti, numéro_unit, nom_apti, nom_unit
+FROM Aptitude, Unité
+WHERE nom_unit = "Illuminor Szeras" AND nom_apti = "Illuminor";
+
 -- Insertion des sorts
 INSERT INTO Sort(id_faction, nom_sort) VALUES (2, "Death");
